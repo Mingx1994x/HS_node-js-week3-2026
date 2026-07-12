e462110ed5db4175a206f4465574530addfc3719
Mingx1994x <mggxmingx@gmail.com>
2026-07-12 11:19:24 +0800

    feat: 完成任務五 上傳圖片 API

--- routes/uploadImage.js
+++ routes/uploadImage.js
@@ -25,5 +25,29 @@ const router = express.Router();
 /* 作答區
 router.METHOD('PATH', (req, res) => { ... });
 */
+router.post('/', (req, res) => {
+
+  const form = formidable({
+    uploadDir,
+    maxFileSize,
+    keepExtensions: true,
+  });
+
+  form.parse(req, (err, _fields, files) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    }
+    const file = files.image?.[0];
+    if (!file) {
+      res.status(400).json({ error: 'No file uploaded' });
+    }
+
+    res.status(200).json({
+      filename: file.originalFilename,
+      sizeKB: Math.round(file.size / 1024),
+      savedPath: file.filepath
+    })
+  });
+})
 
 module.exports = router;
